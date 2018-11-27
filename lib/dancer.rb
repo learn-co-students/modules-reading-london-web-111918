912c6f5741471e0fd80321806a8e82a1249bed52
atkinsonholly <atkinsonholly@gmail.com>
2018-11-27 15:45:37 +0000

    Code along completed

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  extend MetaDancing
+  include Dance #'include' keyword allows our classes to use all of the methods
+  #of the included module as instance methods
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+end
